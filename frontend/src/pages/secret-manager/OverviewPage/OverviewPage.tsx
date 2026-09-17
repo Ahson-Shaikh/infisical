1d52bff3265103c8b207cf35c07481f02ee9b146
andrewhuhh <ananhuang1@gmail.com>
2026-09-17 23:39:04 +0000

    feat(secrets): redesign move destination picker

--- frontend/src/pages/secret-manager/OverviewPage/OverviewPage.tsx
+++ frontend/src/pages/secret-manager/OverviewPage/OverviewPage.tsx
@@ -3927,6 +3927,7 @@ const OverviewPageContent = () => {
         environments={userAvailableEnvs}
         visibleEnvs={visibleEnvs}
         projectId={projectId}
+        projectName={currentProject.name}
         projectSlug={projectSlug}
         sourceSecretPath={secretPath}
         secrets={{}}
